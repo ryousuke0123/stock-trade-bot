075c194904690f3a13a78e77cef9932d1d3ae867
ryousuke0123 <ryousuke3610@gmail.com>
2025-05-13 20:27:22 +0900

    FASTAPI作った

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
